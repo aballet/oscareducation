39deaa88bea7a2c8edb0560e5d10a37824b9191f
maximedimi <maximedimi@hotmail.com>
2017-11-17 22:22:44 +0100

    First unit tests for Skills

--- skills/tests.py
+++ skills/tests.py
@@ -1,68 +1,98 @@
-# -*- coding: utf-8 -*-
 import unittest
-from mock import patch
-
-#from __future__ import unicode_literals
 
 from django.test import TestCase
-from django.conf import settings
-from skills.models import StudentSkill
+from django.contrib.auth.models import User
+from users.models import Professor, Student
+from skills.models import Skill, Relations, StudentSkill, SkillHistory
+from promotions.models import Lesson, Stage
+from exceptions import TypeError, ValueError
+from django.utils import timezone
 
-# Create your tests here.
-
-class TestSkills(TestCase):
-    '''
-    Changez le nom de classe avec un truc plus précis.
-    '''
-
+class SkillsTests(TestCase):
     def setUp(self):
-        settings.configure()
-        '''Initialization de vos tests'''
-        patcher_StudentSkill = patch(StudentSkill)
-        set = patcher_StudentSkill.objects.all()
-        #remove = set.exclude(is_objective = None)
-        test_student = set.first().student
-        test_student_skills = set.filter(student=test_student)
-        test_objective_skills = test_student_skills.exclude(is_objective=None)
+        self.student_user = User.objects.create(username="student1")
+        self.student = Student.objects.create(user=self.student_user, is_pending=False)
+        self.prof_user = User.objects.create(username="professor1")
+        self.prof = Professor.objects.create(user=self.prof_user, is_pending=False)
 
+        self.skill_tab = []
+        self.stud_skill_tab = []
+        for i in range(0,4):
+            self.skill_tab.append(Skill.objects.create(code="u"+str(i), name="Skill"+str(i)))
+            self.stud_skill_tab.append(StudentSkill.objects.create(student=self.student, skill=self.skill_tab[i]))
 
-    def tearDown(self):
-        '''De-init apres chaque test'''
-        pass
+        random_skill = Skill.objects.create(code="u9", name="Skill9")
 
-    def test_example(self):
-        un   = 1
-        deux = 2
-        self.assertEqual(un + un, deux)
+        self.relation1 = Relations.objects.create(from_skill=self.skill_tab[0], to_skill=self.skill_tab[1], relation_type="depend_on")
 
-    def test_example2(self):
-        un   = 1
-        deux = 2
-        self.assertEqual(un + un + un, deux)
+        #self.stud_skill1 = StudentSkill.objects.create(student=self.student, skill=self.skill_tab[0])
 
-    def test_no_objective(self):
-        settings.configure()
-        patcher_StudentSkill = patch(StudentSkill)
-        set = patcher_StudentSkill.objects.all()
-        # remove = set.exclude(is_objective = None)
-        test_student = set.first().student
-        test_student_skills = set.filter(student=test_student)
-        test_objective_skills = test_student_skills.exclude(is_objective=None)
+        self.stage1 = Stage.objects.create(id=1, name="Stage1", short_name="sta", level=1, skills=[])
+        self.lesson1 = Lesson.objects.create(id=1, name="Lesson1", students=[self.student], professors=[self.prof], stage=self.stage1)
 
-        for test_objective_skill in test_objective_skills:
-            patcher_StudentSkill.remove_objective(test_objective_skill)
-        self.assertEqual(patcher_StudentSkill.objects.filter(student=test_student, is_objective=True),Queryset())
+    def test_skill(self):
+        self.assertEquals(self.skill_tab[0], self.skill_tab[0])
 
-    def test_one_objective(self):
-        settings.configure()
-        patcher_StudentSkill = patch(StudentSkill)
-        set = patcher_StudentSkill.objects.all()
-        # remove = set.exclude(is_objective = None)
-        test_student = set.first().student
-        test_student_skills = set.filter(student=test_student)
-        test_objective_skills = test_student_skills.exclude(is_objective=None)
+    def test_dependance_1skill(self):
+        dep = self.skill_tab[1].get_depending_skills()
+        self.assertEquals(dep[0], self.skill_tab[0])
+        self.assertEquals(len(dep), 1)
 
-        first_objective = test_student_skills.first()
-        patcher_StudentSkill.set_objective(first_objective)
-        self.assertEqual(patcher_StudentSkill.objects.filter(student=test_student, is_objective=True).first(),first_objective)
-        #TODO assert children
+    def test_prerequisites_1skill(self):
+        pre = self.skill_tab[0].get_prerequisites_skills()
+        self.assertTrue(self.skill_tab[1] in pre)
+
+    def test_dependance_2skills(self):
+        relation2 = Relations.objects.create(from_skill=self.skill_tab[2], to_skill=self.skill_tab[1], relation_type="depend_on")
+        dep = self.skill_tab[1].get_depending_skills()
+        self.assertTrue(self.skill_tab[0] in dep)
+        self.assertTrue(self.skill_tab[2] in dep)
+        self.assertFalse(self.skill_tab[1] in dep)
+
+    def test_not_recommended(self):
+        for stud_skill in self.stud_skill_tab:
+            self.assertFalse(stud_skill.recommended_to_learn())
+
+    def test_set_objective_1(self):
+        self.stud_skill_tab[1].set_objective(self.prof_user, "because", self.lesson1)
+        self.assertTrue(self.stud_skill_tab[1].recommended_to_learn())
+        self.assertFalse(self.stud_skill_tab[0].recommended_to_learn())
+        self.assertFalse(self.stud_skill_tab[2].recommended_to_learn())
+        self.assertFalse(self.stud_skill_tab[3].recommended_to_learn())
+
+    def test_remove_objective_1(self):
+        self.stud_skill_tab[1].set_objective(self.prof_user, "because", self.lesson1)
+        self.assertTrue(self.stud_skill_tab[1].recommended_to_learn())
+        self.stud_skill_tab[1].remove_objective(self.prof_user, "because", self.lesson1)
+        self.assertFalse(self.stud_skill_tab[1].recommended_to_learn())
+
+    def test_set_objective_2(self):
+        self.stud_skill_tab[0].set_objective(self.prof_user, "because", self.lesson1)
+        self.assertTrue(self.stud_skill_tab[0].recommended_to_learn())
+        self.assertTrue(self.stud_skill_tab[1].recommended_to_learn())
+        self.assertFalse(self.stud_skill_tab[2].recommended_to_learn())
+
+    def test_cant_add_objective(self):
+        for stud_skill in self.stud_skill_tab:
+            self.assertFalse(stud_skill.cant_add_objective())
+
+        self.stud_skill_tab[0].set_objective(self.prof_user, "because", self.lesson1)
+
+        for stud_skill in self.stud_skill_tab:
+            self.assertFalse(stud_skill.cant_add_objective())
+
+        self.stud_skill_tab[1].set_objective(self.prof_user, "because", self.lesson1)
+
+        for stud_skill in self.stud_skill_tab:
+            self.assertFalse(stud_skill.cant_add_objective())
+
+        self.stud_skill_tab[2].set_objective(self.prof_user, "because", self.lesson1)
+
+        for stud_skill in self.stud_skill_tab:
+            self.assertTrue(stud_skill.cant_add_objective())
+
+    def test_depth_sort_skills(self):
+        for stud_skill in self.stud_skill_tab:
+            stud_skill.set_objective(self.prof_user, "because", self.lesson1)
+        sorted_list = StudentSkill.__depth_sort_skills__(self.stud_skill_tab)
+        self.assertEquals(len(sorted_list), 3)
