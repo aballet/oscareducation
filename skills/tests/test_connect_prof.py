2bf38988aabfd1e0ae37b7ae79ab1a38510f284c
Adrien Ballet <adrien.ballet@student.uclouvain.be>
2017-12-12 15:38:31 +0100

    Selenium tests

--- skills/tests/test_connect_prof.py
+++ skills/tests/test_connect_prof.py
@@ -1,10 +1,10 @@
-import time
-from SetUp import SetUp
-
-
-class TestConnectProf(SetUp):
-
-    def test_connect(self):
-        self.connect_prof()
-        time.sleep(3)
-        self.disconnect()
+# import time
+# from SetUp import SetUp
+#
+#
+# class TestConnectProf(SetUp):
+#
+#     def test_connect(self):
+#         self.connect_prof()
+#         time.sleep(1000)
+#         self.disconnect()
---
+++ skills/tests/test_set_goals_individually.py
@@ -0,0 +1,77 @@
+import time
+from SetUp import SetUp
+
+
+class TestSetGoalsIndividually(SetUp):
+
+    # def test_set_goals(self):
+    #     self.connect_prof()
+    #     self.browser.find_element_by_xpath('/html/body/div[2]/div[2]/div/div[2]/div[4]/div/a').click()
+    #     time.sleep(5)
+    #     self.browser.find_element_by_xpath(
+    #         '/html/body/div[2]/div[2]/div/div[1]/div[4]/div/table/tbody/tr[1]/td[1]/a').click()
+    #     time.sleep(5)
+    #     self.browser.find_element_by_xpath('/html/body/div[2]/div[2]/ul/li[3]/a').click()
+    #     time.sleep(5)
+    #     self.browser.find_element_by_xpath(
+    #         '/html/body/div[2]/div[2]/div[4]/div/div[3]/div/div/div[2]/div/ul/li[1]/a').click()
+    #
+    #     # TODO : find a way to select the buttons on the popup
+    #
+    #     time.sleep(5)
+    #     self.disconnect()
+    #
+    # def test_skill_colors(self):
+    #     self.student_skill11.validate(self.user_prof, "Test purpose", self.lesson)
+    #     self.student_skill12.unvalidate(self.user_prof, "Test purpose", self.lesson)
+    #     self.student_skill13.set_objective(self.user_prof, "Test purpose", self.lesson)
+    #     self.connect_prof()
+    #     self.browser.find_element_by_xpath('/html/body/div[2]/div[2]/div/div[2]/div[4]/div/a').click()
+    #     time.sleep(5)
+    #     self.browser.find_element_by_xpath(
+    #         '/html/body/div[2]/div[2]/div/div[1]/div[4]/div/table/tbody/tr[1]/td[1]/a').click()
+    #     time.sleep(5)
+    #     self.browser.find_element_by_xpath('/html/body/div[2]/div[2]/ul/li[3]/a').click()
+    #     time.sleep(5)
+    #     elem_class = self.browser.find_element_by_xpath(
+    #         '/html/body/div[2]/div[2]/div[4]/div/div[3]/div/div/div[2]/div/ul/li[1]/a').get_attribute("class")
+    #     elem_class_tab = elem_class.split(" ")
+    #
+    #     success_button = False
+    #     for e in elem_class_tab:
+    #         if e.find("btn-success") != -1:
+    #             success_button = True
+    #     assert success_button, True
+    #
+    #     elem_class = self.browser.find_element_by_xpath(
+    #         '/html/body/div[2]/div[2]/div[4]/div/div[3]/div/div/div[2]/div/ul/li[2]/a').get_attribute("class")
+    #     elem_class_tab = elem_class.split(" ")
+    #
+    #     success_button = False
+    #     for e in elem_class_tab:
+    #         if e.find("btn-warning") != -1:
+    #             success_button = True
+    #     assert success_button, True
+    #
+    #     elem_class = self.browser.find_element_by_xpath(
+    #         '/html/body/div[2]/div[2]/div[4]/div/div[3]/div/div/div[2]/div/ul/li[3]/a').get_attribute("class")
+    #     elem_class_tab = elem_class.split(" ")
+    #
+    #     success_button = False
+    #     for e in elem_class_tab:
+    #         if e.find("btn-primary") != -1:
+    #             success_button = True
+    #     assert success_button, True
+    #
+    #     elem_class = self.browser.find_element_by_xpath(
+    #         '/html/body/div[2]/div[2]/div[4]/div/div[3]/div/div/div[2]/div/ul/li[4]/a').get_attribute("class")
+    #     elem_class_tab = elem_class.split(" ")
+    #
+    #     success_button = False
+    #     for e in elem_class_tab:
+    #         if e.find("btn-info") != -1:
+    #             success_button = True
+    #     assert success_button, True
+    #
+    #     time.sleep(5)
+    #     self.disconnect()
