c04502c840683f871bed917ee2f3b5717b145e9d
Adrien Ballet <adrien.ballet@student.uclouvain.be>
2017-12-13 15:30:27 +0100

    added import

--- skills/models.py
+++ skills/models.py
@@ -10,6 +10,7 @@ from django.contrib.auth.models import User
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.db.models import Q
 
 from examinations.models import Context
 
